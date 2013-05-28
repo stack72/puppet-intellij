329e10b78126ea18e19218e688bcd48ff0871389
stack72 <public@paulstack.co.uk>
2013-05-28 17:19:22 +0100

    fixing up the intellij module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,5 @@
-class webstorm ($version = '12.1.3') {
+class intellij {
+  $version = '12.1.3'
   package { 'IntelliJ':
     provider => 'appdmg',
     source   => "http://download.jetbrains.com/idea/ideaIU-${version}.dmg",
